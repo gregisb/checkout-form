cf3ad4edc6928df06439fe1a2a7d835d64387067
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-07-13 22:20:39 -0300

    creates form view home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <CheckoutForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CheckoutForm from '@/components/CheckoutForm.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    CheckoutForm
   }
 }
 </script>
